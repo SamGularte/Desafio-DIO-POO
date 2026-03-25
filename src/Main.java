f129ef98a4681ced2d8adbe90fa36153a3d404a5
samuel gularte <samuelgulartee@gmail.com>
2026-03-25 16:25:21 -0300

    Mentoria and Curso classes added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import dominio.Curso;
+import dominio.Mentoria;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    Curso curso1 = new Curso("curso java", "descricao curso java", 8);
+    Curso curso2 = new Curso("curso js", "descricao curso js", 4);
+    Mentoria mentoria1 = new Mentoria("Mentoria java", "descricao mentoria java", LocalDate.now());
+    Mentoria mentoria2 = new Mentoria("Mentoria js", "descricao mentoria js", LocalDate.now());
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+
+    System.out.println(curso1);
+    System.out.println(curso2);
+    System.out.println(mentoria1);
+    System.out.println(mentoria2);
 }
